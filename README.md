72e53d884af0f65fb358f547ff2f6b8962829484
Emmanuel <opensoftweb@gmail.com>
2021-08-01 18:04:31 +0100

    set yarn version to berry

---
+++ README.md
@@ -0,0 +1,2 @@
+# UI Components
+A set of customizable and strongly typed opensource reusable UI kits
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components/macro";
+import { typography, color, space } from "styled-system";
+
+export const Button = () => {
+  return <div></div>;
+};
