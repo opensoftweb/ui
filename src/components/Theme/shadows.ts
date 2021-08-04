6471854c1279d21b873de60a072e6944fa5ce9aa
Emmanuel <opensoftweb@gmail.com>
2021-08-04 21:09:51 +0100

    add theme shadows

---
+++ src/components/Theme/shadows.ts
@@ -0,0 +1,8 @@
+export const shadows = {
+  none: "none",
+  sm: "0 .125rem .25rem rgba(0, 0, 0, .075)",
+  regular: "0 .5rem 1rem rgba(0, 0, 0, .15)",
+  lg: "0 1rem 3rem rgba(0, 0, 0, .175)",
+};
+
+export type Shadows = typeof shadows;
