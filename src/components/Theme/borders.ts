29163f904f3439e2f883322cad4fd6d9fdfcdcca
Emmanuel <opensoftweb@gmail.com>
2021-08-04 16:11:09 +0100

    add theme borders

---
+++ src/components/Theme/borders.ts
@@ -0,0 +1,49 @@
+export const borders = {
+  none: 0,
+  hidden: "hidden",
+
+  "1px-dashed": "1px dashed",
+  "2px-dashed": "2px dashed",
+  "4px-dashed": "4px dashed",
+
+  "1px-dotted": "1px dotted",
+  "2px-dotted": "2px dotted",
+  "4px-dotted": "4px dotted",
+
+  "1px-double": "1px double",
+  "2px-double": "2px double",
+  "4px-double": "4px double",
+
+  "1px-groove": "1px groove",
+  "2px-groove": "2px groove",
+  "4px-groove": "4px groove",
+
+  "1px-ridge": "1px ridge",
+  "2px-ridge": "2px ridge",
+  "4px-ridge": "4px ridge",
+
+  "1px-inset": "1px inset",
+  "2px-inset": "2px inset",
+  "4px-inset": "4px inset",
+
+  "1px-outset": "1px outset",
+  "2px-outset": "2px outset",
+  "4px-outset": "4px outset",
+
+  "1px-solid": "1px solid",
+  "2px-solid": "2px solid",
+  "4px-solid": "4px solid",
+};
+
+export const borderWidths = {
+  0: "0",
+  1: "1px",
+  2: "2px",
+  3: "3px",
+  4: "4px",
+  5: "5px",
+  6: "6px",
+};
+
+export type BorderWidths = typeof borderWidths;
+export type Borders = typeof borders;
