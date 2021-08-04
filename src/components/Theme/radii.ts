62d0c9f8793481846141b74eae87a19ab79fb205
Emmanuel <opensoftweb@gmail.com>
2021-08-04 20:29:30 +0100

    add theme radii

---
+++ src/components/Theme/radii.ts
@@ -0,0 +1,11 @@
+export const radii = {
+  circle: "50%",
+  squared: "0",
+  rounded: ".25rem",
+  "rounded-1": ".2rem",
+  "rounded-2": ".25rem",
+  "rounded-3": ".3rem",
+  pill: "50rem",
+};
+
+export type Radii = typeof radii;
