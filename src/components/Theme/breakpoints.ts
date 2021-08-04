06073f18f836df85adc3fd720f4170e813a0abb0
Emmanuel <opensoftweb@gmail.com>
2021-08-04 20:25:24 +0100

    add theme breakpoints

---
+++ src/components/Theme/breakpoints.ts
@@ -0,0 +1,13 @@
+/**
+ * Define your breakpoints
+ * using Bootstrap standard
+ */
+export const breakpoints = {
+  sm: "576px",
+  md: "768px",
+  lg: "992px",
+  xl: "1200px",
+  xxl: "1400px",
+};
+
+export type BreakPoints = typeof breakpoints;
