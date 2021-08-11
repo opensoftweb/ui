0127c16796023dc60e84b1a966a6181ddf7bd337
Emmanuel <opensoftweb@gmail.com>
2021-08-11 15:45:39 +0100

    add: JustifyContent component

---
+++ src/components/JustifyContent/index.ts
@@ -0,0 +1,13 @@
+export const JustifyContent = {
+  stretch: "justify-content: stretch;",
+  center: "justify-content: center;",
+  start: "justify-content: flex-start;",
+  end: "justify-content: flex-end;",
+  between: "justify-content: space-between;",
+  around: "justify-content: space-around;",
+  initial: "justify-content: initial;",
+  inherit: "justify-content: inherit;",
+};
+
+export type JustifyContentType = typeof JustifyContent;
+export type IJustifyContent = keyof JustifyContentType;
